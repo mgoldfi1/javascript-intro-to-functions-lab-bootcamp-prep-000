0bfb1b80da13580d62d4382bc6c15e9934753e04
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-06 21:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,3 @@ function whisper(string) {
 function logShout(string) {console.log(string.toUpperCase())
 }
 function logWhisper(string){console.log(string.toLowerCase())}
-
-function sayHiToGrandma(string) {return grandma}
